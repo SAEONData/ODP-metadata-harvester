90461f4672165c61701537018ffb94af1ec022eb
kyle-SAEON <kyle@saeon.ac.za>
2020-08-13 13:12:11 +0200

    Feat: Changed HarvestController.py to use the ODPClient for creating and updating records

--- HarvestController.py
+++ HarvestController.py
@@ -1,12 +1,21 @@
 import metadataHarvestor
-#import ODPClient
+from odp.client import ODPClient
+from odp.exceptions import ODPException
 import pprint
 
 class HarvestController:
 
     def harvest_records(self):
         #Harvest records from a file and push them to a an API
-        harvestor = metadataHarvestor.MetadataHarvest()
-        for record in harvestor.get_records():
-            pprint.pprint(record)
-            #self.odp_client.create_record(record)
+        harvester = metadataHarvestor.MetadataHarvest()
+        client = ODPClient()
+        for record in harvester.get_records():
+            try:
+                result = client.create_or_update_metadata_record('chief-directorate-oceans-and-coastal-research',
+                                                                 'marine-information-management-system-collection',
+                                                                 'sans-1878-mims-historical-1',
+                                                                 record,capture_method='harvester',
+                                                                 data_agreement_url='https://www.environment.gov.za/branches/oceans_coast')
+                #pprint(result, indent=4)
+            except ODPException as e:
+                print(f"{e}: {e.error_detail}")
